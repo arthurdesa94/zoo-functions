2ea40ef0eef821bc71b6484dcaca23b7e578f55a
Arthur de Sa <arthurdesa94@gmail.com>
2020-08-26 18:01:40 -0300

    acicionando a entryCalculator cc3

--- src/zoo.js
+++ src/zoo.js
@@ -62,7 +62,7 @@ function animalCount(species) {
 function entryCalculator(entrants) {
   if (!entrants || entrants === {}) return 0;
   return Object.keys(entrants)
-  .reduce((total, ticketPrice) => 
+  .reduce((total, ticketPrice) =>
     total + (entrants[ticketPrice] * data.prices[ticketPrice]), 0);
 }
 

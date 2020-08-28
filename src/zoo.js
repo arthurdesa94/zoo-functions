ac41ec49987f0a388a8b9b9ccec49dcaa71ac14d
Arthur de Sa <arthurdesa94@gmail.com>
2020-08-27 22:41:25 -0300

    13 CC10

--- src/zoo.js
+++ src/zoo.js
@@ -125,16 +125,19 @@ function employeeCoverage(idOrName) {
   } else {
     // [{}]
     filteredEmployees = data.employees
-    .filter((employee) => 
-      employee.id === idOrName || employee.firstName === idOrName || employee.lastName === idOrName);
-  }
+    .filter((employee) =>
+      employee.id === idOrName || 
+      employee.firstName === idOrName || 
+      employee.lastName === idOrName
+      );
+    }
   filteredEmployees.forEach((employee) => {
     const mappedAnimals = employee.responsibleFor
     .map((animalIdResponsableFor) => {
       const foundAnimalName = data.animals
       .find(animal => animal.id === animalIdResponsableFor).name;
       return foundAnimalName;
-    }
+    };
     );
     result[`${employee.firstName} ${employee.lastName}`] = mappedAnimals;
   });
